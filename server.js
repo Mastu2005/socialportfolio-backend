c1f3fb5dd1b4f4f694acf3cb3433f56dc87d7a24
Mastu2005 <12345678+Mastu2005@users.noreply.github.com>
2026-01-05 14:07:38 +0530

    fix cors via adding https in  Vercel URL

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "socialportfolio-frontend-i7ce1agsw-mastu2005s-projects.vercel.app"
+    "https://socialportfolio-frontend-i7ce1agsw-mastu2005s-projects.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
